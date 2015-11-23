c9118f455f1f312e897a4cfcfa46b412eae2316a
wangkangkang <wangkangkang@x-one.cc>
2015-11-23 20:04:44 +0800

    pacman 前后端联调

--- js/index.js
+++ js/index.js
@@ -8,25 +8,19 @@
      * 变量初始化
      * @type {string}
      */
-    var originStep     = getURLParam('start'),
-        targetStep     = getURLParam('end'),
-        OpenID         = getURLParam('OpenID'),
-        activityID     = getURLParam('activityID'),
-        isJoinedMember = getURLParam('isNewUser') != '' && getURLParam('isNewUser') != -1,
-        gameContainer  = $('.gameContainer'),
-        stepsPrefix    = 'step'/*,
-         totalSteps  = 5*/;
+    var OpenID     = getURLParam('OpenID'),
+        activityID = getURLParam('activityID');
 
     /**
      * 让pacman前进到指定的步骤
-     * @param targetStep [Number] 要前进到的步骤
+     * @param mockData
      */
-    function gotoStep(originStep, targetStep) {
+    function gotoStep(mockData) {
         // init
-        var stepStart        = stepsPrefix + originStep,
-            stepEnd          = stepsPrefix + targetStep,
-            gift             = gameContainer.find('.gift' + targetStep),
-            hasGift          = gift.length > 0,
+        var gameContainer    = $('.gameContainer'),
+            stepStart        = 'step' + mockData.data.currentAction.start,
+            stepEnd          = 'step' + mockData.data.currentAction.end,
+            hasGift          = mockData.data.currentAction.hasGift,
             character        = gameContainer.find('.character'),
             pacman           = gameContainer.find('.character .pacman'),
             pacman_frontface = gameContainer.find('.character .pacman-frontface');
@@ -90,18 +84,61 @@
         /**
          * 页面加载时, 根据url param指定的step, 移动到目标位置
          */
-        // 向后端发送用户输入的信息, 获取后端返回的兑换码
+            // 向后端发送用户输入的信息, 获取后端返回的兑换码
         $.ajax({
             /*url     : "api/getActivityInfo_newUser.json",*/
-            url     : "http://123.56.8.204:81/Customer/GetActivityDetail",
+            url     : "http://123.56.8.204/Customer/GetActivityDetail",
             data    : {
                 "OpenID"    : OpenID,
                 "activityID": activityID
             },
             success : function (result) {
-                if (originStep != -1 && targetStep != -1) {
-                    gotoStep(originStep, targetStep);
-                }
+
+                var mockData = {
+                    "code"   : "0",
+                    "message": "",
+                    "data"   : {
+                        "user"         : {
+                            "isNewUser" : true,
+                            "OpenID"    : "o6_bmjrPTlm6_2sgVt7hMZOPfL2M",
+                            "nickname"  : "Band",
+                            "sex"       : 1,
+                            "headimgurl": "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/0"
+                        },
+                        "currentAction": {
+                            "start"  : 0,
+                            "end"    : 1,
+                            "hasGift": true
+                        },
+                        "giftList"     : [
+                            {
+                                "giftInfo": {
+                                    "name"       : "橙汁",
+                                    "description": "柑橘类果汁，特别是橙汁中的黄酮能有效抑制乳腺癌、肺癌等细胞的增生。经常饮用橙汁也可以有效预防某些慢性疾病、维持心肌功能以及降低血压。研究显示，每天喝3杯橙汁可以增加体内高密度脂蛋白（HDL）的含量，从而降低患心脏病的可能。此外，在服药期间吃一些橙子或饮橙汁，可增加机体对药物的吸收量，从而使药效加倍。",
+                                    "photo"      : "http://p0.55tuanimg.com/static/goods/ckeditor/2013/02/04/17/ckeditor_1359971186_8668_wm.jpg"
+                                }
+                            },
+                            {},
+                            {
+                                "giftInfo": {
+                                    "name"       : "蓝莓汁",
+                                    "description": "蓝莓被联合国粮农组织列为人类五大健康食品之一，被誉为“21世纪功能性保健浆果”，蓝莓果汁含有丰富的维生素和氨基酸，蓝莓果汁含有丰富的花青素，具有清除氧自由基、保护视力、延缓脑神经衰老、提高记忆力的作用。由于蓝莓对保护和增强视力的独到效果，蓝莓又被称为“飞行员的早餐”，是英、美空军指定的飞行员指定早餐食品。",
+                                    "photo"      : "http://p1.meituan.net/deal/fbd19498af14fa6f63aa4be2c54995e653001.jpg"
+                                }
+                            },
+                            {},
+                            {
+                                "giftInfo": {
+                                    "name"       : "卡布奇诺",
+                                    "description": "20世纪初期，意大利人阿奇布夏发明蒸汽压力咖啡机的同时，也发展出了卡布奇诺咖啡。卡布奇诺是一种加入以同量的意大利特浓咖啡和蒸汽泡沫牛奶相混合的意大利咖啡。此时咖啡的颜色，就像卡布奇诺教会的修士在深褐色的外衣上覆上一条头巾一样，咖啡因此得名。",
+                                    "photo"      : "http://c.hiphotos.baidu.com/baike/w%3D268/sign=11a04d3eccfc1e17fdbf8b377290f67c/0b7b02087bf40ad130d03dcb542c11dfa9eccee6.jpg"
+                                }
+                            }
+                        ]
+                    }
+                };
+
+                gotoStep(mockData);
 
             },
             error   : function (XMLHttpRequest, textStatus, errorThrown) {
@@ -123,22 +160,22 @@
                 userName        = inputUserName.val(),
                 userMobile      = inputUserMobile.val();
             /*if ($.trim(userName) == "") {
-                alert('请输入您的名字~');
-                inputUserName.focus();
-                return false;
-            } else if ($.trim(userMobile).length > 5) {
-                alert('您输入的内容过长~');
-                inputUserMobile.focus();
-                return false;
-            } else if ($.trim(userMobile) == "") {
-                alert('请输入您的手机号码, 方便领取礼品~');
-                inputUserMobile.focus();
-                return false;
-            } else if ($.trim(userMobile).length != 11) {
-                alert('请输入正确的11位手机号~');
-                inputUserMobile.focus();
-                return false;
-            }*/
+             alert('请输入您的名字~');
+             inputUserName.focus();
+             return false;
+             } else if ($.trim(userMobile).length > 5) {
+             alert('您输入的内容过长~');
+             inputUserMobile.focus();
+             return false;
+             } else if ($.trim(userMobile) == "") {
+             alert('请输入您的手机号码, 方便领取礼品~');
+             inputUserMobile.focus();
+             return false;
+             } else if ($.trim(userMobile).length != 11) {
+             alert('请输入正确的11位手机号~');
+             inputUserMobile.focus();
+             return false;
+             }*/
 
             // 与后端通信之前, 禁用领取按钮, 避免重复提交
             $('.J_GetGift').text('请稍候...').prop('disabled', 'disabled');
